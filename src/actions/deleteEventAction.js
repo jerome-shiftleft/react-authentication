69209fc3635dc89a7fe71824e819a4129404a3ea
jerome-shiftleft <jerome@theshiftleft.com>
2024-04-08 02:09:55 +0800

    Add authorization header to deleteEventAction.js

--- src/actions/deleteEventAction.js
+++ src/actions/deleteEventAction.js
@@ -1,9 +1,15 @@
 import { json, redirect } from "react-router-dom";
+import { getAuthToken } from "../util/auth";
 
 export default async function deleteEventAction({ params, request }) {
   const id = params.id;
+  const token = getAuthToken();
+  console.log("token: ", token);
   const response = await fetch("http://localhost:8080/events/" + id, {
     method: request.method,
+    headers: {
+      "Authorization": "Bearer " + token,
+    },
   });
 
   if (!response.ok) {
