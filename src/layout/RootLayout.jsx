6a6042a9d24f03eacfe7b278152e243ad5e5905e
jerome-shiftleft <jerome@theshiftleft.com>
2024-04-14 05:46:17 +0800

    force logout after 1 hour

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,15 +1,31 @@
-import { Outlet, useNavigation } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useLoaderData, useSubmit } from "react-router-dom";
 import MainNavigation from "../components/MainNavigation";
 
 export default function RootLayout() {
-  //const navigation = useNavigation();
   //console.log("data state: ", navigation.state);
+  const token = useLoaderData();
+  const submit = useSubmit();
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    // Force Logout after 1 hour    
+    const timeout = 60 * 60 * 1000; // 1 hour in milliseconds
+
+    setTimeout(() => {
+      submit(null, { action: "/logout", method: "post" });
+    }, timeout);
+  }, [token, submit]);
+
   return (
     <>
       <MainNavigation />
       <main>
         {/* {navigation.state === "loading" && <p>Loading...</p>} */}
-        <Outlet />        
+        <Outlet />
       </main>
     </>
   );
